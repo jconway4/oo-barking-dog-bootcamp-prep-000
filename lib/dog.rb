88b1dfeb0eb517dc85c72f6fa69895574ee86577
jconway4 <jconwa4@illinois.edu>
2018-03-26 21:27:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,15 +3,15 @@ class Dog
   def initialize(name)
     @name = name
   end
-  
+
   def name
     @name
   end
-  
   def name=(new_name)
     @name = new_name
   end
   
-  
+  def bark
+    puts "woof!"
   
 end
